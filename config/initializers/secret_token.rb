100123e4cd1b66e6cfefb3d24d308797b09b41b7
moredream <moredream7@gmail.com>
2014-04-09 13:28:18 +0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Secondlife::Application.config.secret_key_base = '255b4f578bdfe442fccd8d605937a1bf989a36aa02aef6f4995d98b9d17ec307ffce210a4632189ac93533c9f08a3fff884bd29df9805a0ab6e393b5245224ad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Secondlife::Application.config.session_store :cookie_store, key: '_secondlife_session'
